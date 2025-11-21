8fb0c0e984def3a9904ccdfd9cee56991af8a477
Erkan <erkanozhan@gmail.com>
2025-11-21 10:14:58 +0300

    Updated

--- Prophet.py
+++ Prophet.py
@@ -2,9 +2,11 @@ from pmdarima.datasets import load_airpassengers
 import pandas as pd
 from prophet import Prophet
 
-# Veriyi yükle
-df = load_airpassengers
+import pandas as pd
+from prophet import Prophet
 
+# Veriyi yükle
+df = pd.read_csv('AirPassengers.csv')
 
 # Prophet formatına uygun isimlendirme
 df.columns = ['ds', 'y']
