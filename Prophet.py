800d4a6c571b195957b07e100c585c1e49e68c0c
Erkan <erkanozhan@gmail.com>
2025-11-21 10:31:52 +0300

    Updated

--- Prophet.py
+++ Prophet.py
@@ -13,3 +13,17 @@ df.columns = ['ds', 'y']
 # Tarih formatını datetime objesine çevirme
 df['ds'] = pd.to_datetime(df['ds'])
 
+# Modeli başlat
+m = Prophet(yearly_seasonality=True, daily_seasonality=False)
+
+# Modeli veriye uydur (Eğitim)
+m.fit(df)
+
+# Gelecek 12 ay için boş tarih satırları oluştur
+future = m.make_future_dataframe(periods=12, freq='MS') # MS: Month Start
+
+# Tahmin yap
+forecast = m.predict(future)
+
+# Sonuçları incele (ds, yhat, yhat_lower, yhat_upper)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
